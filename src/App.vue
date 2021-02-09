d8c45054659dfeb52819667e5eb70b48c83c689a
teewhy <hamendment@gmail.com>
2021-02-09 15:11:10 +0100

    changed layout
    Co-Authored-By: Paul <abayomipaulhenryhill@gmail.com>

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
   <div id="app">
-        <router-view/>
+        <!-- <router-view/> -->
+        <!-- <Login/> -->
+        <MasterLayout/>
   </div>
 </template>
 
 <script>
 
+import MasterLayout from './components/layout/master.layout'
+import Login from './components/auth/Login'
 
 export default {
+  components:{MasterLayout, Login},
    name: 'App'
 }
 </script>
---
+++ src/components/layout/master.layout.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <Header/>
+
+        <div class="app-wrapper">
+            <div  class="app-content pt-3 p-md-3 p-lg-4">
+                <div class="container-xl">
+                    <router-view/>
+                </div>
+            </div>
+        </div>
+
+        <Footer/>
+    </div>
+</template>
+
+<script>
+import Header from './Header'
+import Footer from './Footer'
+export default {
+components:{Header,Footer}
+}
+</script>
+
+<style>
+
+</style>
