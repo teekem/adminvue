584ce9d4f317b7af32064fab09b113b42f9bcb8b
Paul <abayomipaulhenryhill@gmail.com>
2021-05-21 19:03:44 +0100

    Update course.container.vue

--- src/components/course/course.container.vue
+++ src/components/course/course.container.vue
@@ -4,10 +4,10 @@
       <div class="container container-fluid">
         <div class="card mb-2">
           <div class="card-body">
-            <b-form-group label="Default:" label-cols-sm="2">
+            <b-form-group label="Select File " label-cols-sm="2">
               <b-form-file
                 id="file-default"
-                accept=".xlsx"
+               
                 v-model="uploadData.file"
               ></b-form-file>
             </b-form-group>
@@ -23,11 +23,12 @@
               </button>
             </div>
             <div class="text-center mt-2">
-              <b-progress
-                value="20"
+              <b-progress v-if="uploadData.percentage"
+                :value="uploadData.percentage"
                 max="100"
                 show-progress
                 animated
+                :variant="uploadData.status ? 'success' : 'danger'"
               ></b-progress>
             </div>
           </div>
@@ -47,16 +48,17 @@ export default {
       uploadData: {
         percentage: "",
         file: null,
-        status: "",
+        status: true,
+
       },
     };
   },
   methods: {
     uploadExcel() {
       let formData = new FormData();
-      formData.append("file", this.uploadData.file);
+      formData.append("courses", this.uploadData.file);
       axios
-        .post("/admin/upload/courses", {
+        .post("/admin/upload/courses", formData, {
           headers: { "Content-Type": "multipart/form-data" },
           onUploadProgress: function (progressEvent) {
             this.uploadData.percentage = parseInt(
@@ -65,9 +67,19 @@ export default {
           }.bind(this),
         })
         .then((response) => {
-          this.uploadData.status = "File uploaded"
+          this.uploadData.status = true
+          Toast.fire({
+            icon:"info",
+            title:"Upload Successful"
+          })
         })
-        .catch((err) => {});
+        .catch((err) => {
+          this.uploadData.status = false
+          Toast.fire({
+            icon:"error",
+            title:"Error Uploading courses"
+          })
+        });
     },
   },
 };
